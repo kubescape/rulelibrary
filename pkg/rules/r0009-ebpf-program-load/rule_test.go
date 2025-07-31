d3601b0b640e136d0b12d5ba1f5f127796c41fbd
Amit Schendel <amitschendel@gmail.com>
2025-07-31 13:37:01 +0000

    Adding ebpf rule
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

---
+++ pkg/rules/r0009-ebpf-program-load/rule_test.go
@@ -0,0 +1,123 @@
+package r0009ebpfprogramload
+
+import (
+	"testing"
+
+	"github.com/goradd/maps"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"github.com/kubescape/node-agent/pkg/objectcache"
+	celengine "github.com/kubescape/node-agent/pkg/rulemanager/cel"
+	"github.com/kubescape/node-agent/pkg/rulemanager/profilevalidator"
+	"github.com/kubescape/node-agent/pkg/rulemanager/types"
+	"github.com/kubescape/node-agent/pkg/utils"
+	common "github.com/kubescape/rulelibrary/pkg/common"
+	"github.com/kubescape/storage/pkg/apis/softwarecomposition/v1beta1"
+)
+
+func TestR0009EbpfProgramLoad(t *testing.T) {
+	ruleSpec, err := common.LoadRuleFromYAML("ebpf-program-load.yaml")
+	if err != nil {
+		t.Fatalf("Failed to load rule: %v", err)
+	}
+
+	// Create a syscall event with bpf syscall
+	e := &types.SyscallEvent{
+		Event: eventtypes.Event{
+			CommonData: eventtypes.CommonData{
+				K8s: eventtypes.K8sMetadata{
+					BasicK8sMetadata: eventtypes.BasicK8sMetadata{
+						ContainerName: "test",
+					},
+				},
+				Runtime: eventtypes.BasicRuntimeMetadata{
+					ContainerID: "test",
+				},
+			},
+		},
+		Pid:         1234,
+		Comm:        "test-process",
+		SyscallName: "bpf",
+	}
+
+	objCache := &profilevalidator.RuleObjectCacheMock{
+		ContainerIDToSharedData: maps.NewSafeMap[string, *objectcache.WatchedContainerData](),
+	}
+
+	objCache.SetSharedContainerData("test", &objectcache.WatchedContainerData{
+		ContainerType: objectcache.Container,
+		ContainerInfos: map[objectcache.ContainerType][]objectcache.ContainerInfo{
+			objectcache.Container: {
+				{
+					Name: "test",
+				},
+			},
+		},
+	})
+
+	celEngine, err := celengine.NewCEL(objCache)
+	if err != nil {
+		t.Fatalf("Failed to create CEL engine: %v", err)
+	}
+
+	celSerializer := celengine.CelEventSerializer{}
+	eventMap := celSerializer.Serialize(e)
+
+	// Test without profile - should trigger alert
+	ok, err := celEngine.EvaluateRule(eventMap, utils.SyscallEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation failed - should have detected bpf syscall")
+	}
+
+	// Evaluate the message
+	message, err := celEngine.EvaluateExpression(eventMap, ruleSpec.Rules[0].Expressions.Message)
+	if err != nil {
+		t.Fatalf("Failed to evaluate message: %v", err)
+	}
+	if message != "bpf system call executed in test" {
+		t.Fatalf("Message evaluation failed, got: %s", message)
+	}
+
+	// Evaluate the unique id
+	uniqueId, err := celEngine.EvaluateExpression(eventMap, ruleSpec.Rules[0].Expressions.UniqueID)
+	if err != nil {
+		t.Fatalf("Failed to evaluate unique id: %v", err)
+	}
+	if uniqueId != "test-process_bpf" {
+		t.Fatalf("Unique id evaluation failed, got: %s", uniqueId)
+	}
+
+	// Test with whitelisted bpf syscall in profile
+	profile := objCache.ApplicationProfileCache().GetApplicationProfile("test")
+	if profile == nil {
+		profile = &v1beta1.ApplicationProfile{}
+		profile.Spec.Containers = append(profile.Spec.Containers, v1beta1.ApplicationProfileContainer{
+			Name:     "test",
+			Syscalls: []string{"bpf"},
+		})
+
+		objCache.SetApplicationProfile(profile)
+	}
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.SyscallEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if ok {
+		t.Fatalf("Rule evaluation should have failed since bpf syscall is whitelisted")
+	}
+
+	// Test with non-bpf syscall (should not trigger)
+	e.SyscallName = "open"
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.SyscallEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if ok {
+		t.Fatalf("Rule evaluation should have failed for non-bpf syscall")
+	}
+}
