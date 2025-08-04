48d55680dcad08a6e85ab125f27d1dc1e0cac110
Afek Berger <afekb@armosec.io>
2025-08-04 11:03:14 +0300

    added ptrace, hardlink & ld_preload rules
    Signed-off-by: Afek Berger <afekb@armosec.io>

---
+++ pkg/rules/r1012-hardlink-created-over-sensitive-file/rule_test.go
@@ -0,0 +1,172 @@
+package r1012hardlinkcreatedoversensitivefile
+
+import (
+	"testing"
+	"time"
+
+	"github.com/goradd/maps"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"github.com/kubescape/node-agent/pkg/config"
+	tracerhardlinktype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/hardlink/types"
+	"github.com/kubescape/node-agent/pkg/objectcache"
+	objectcachev1 "github.com/kubescape/node-agent/pkg/objectcache/v1"
+	"github.com/kubescape/node-agent/pkg/rulemanager"
+	celengine "github.com/kubescape/node-agent/pkg/rulemanager/cel"
+	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/cache"
+	"github.com/kubescape/node-agent/pkg/utils"
+	common "github.com/kubescape/rulelibrary/pkg/common"
+	"github.com/kubescape/storage/pkg/apis/softwarecomposition/v1beta1"
+)
+
+func TestR1012HardlinkCreatedOverSensitiveFile(t *testing.T) {
+	ruleSpec, err := common.LoadRuleFromYAML("hardlink-created-over-sensitive-file.yaml")
+	if err != nil {
+		t.Fatalf("Failed to load rule: %v", err)
+	}
+
+	// Create a hardlink event
+	e := &tracerhardlinktype.Event{
+		Event: eventtypes.Event{
+			CommonData: eventtypes.CommonData{
+				K8s: eventtypes.K8sMetadata{
+					BasicK8sMetadata: eventtypes.BasicK8sMetadata{
+						ContainerName: "test",
+					},
+				},
+				Runtime: eventtypes.BasicRuntimeMetadata{
+					ContainerID: "test",
+				},
+			},
+		},
+		Comm:    "test",
+		OldPath: "test",
+		NewPath: "test",
+	}
+
+	objCache := &objectcachev1.RuleObjectCacheMock{
+		ContainerIDToSharedData: maps.NewSafeMap[string, *objectcache.WatchedContainerData](),
+	}
+
+	objCache.SetSharedContainerData("test", &objectcache.WatchedContainerData{
+		ContainerType: objectcache.Container,
+		ContainerInfos: map[objectcache.ContainerType][]objectcache.ContainerInfo{
+			objectcache.Container: {
+				{
+					Name: "test",
+				},
+			},
+		},
+	})
+
+	celEngine, err := celengine.NewCEL(objCache, config.Config{
+		CelConfigCache: cache.FunctionCacheConfig{
+			MaxSize: 1000,
+			TTL:     1 * time.Microsecond,
+		},
+	})
+	if err != nil {
+		t.Fatalf("Failed to create CEL engine: %v", err)
+	}
+
+	celSerializer := celengine.CelEventSerializer{}
+
+	eventMap := celSerializer.Serialize(e)
+
+	// Evaluate the rule - should not trigger for non-sensitive file
+	ok, err := celEngine.EvaluateRule(eventMap, utils.HardlinkEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if ok {
+		t.Fatalf("Rule evaluation should not trigger for non-sensitive file")
+	}
+
+	// Test with sensitive file path
+	e.OldPath = "/etc/shadow"
+	e.NewPath = "/etc/abc"
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.HardlinkEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation should trigger for sensitive file")
+	}
+
+	// Evaluate the message
+	message, err := celEngine.EvaluateExpression(eventMap, ruleSpec.Rules[0].Expressions.Message)
+	if err != nil {
+		t.Fatalf("Failed to evaluate message: %v", err)
+	}
+	expectedMessage := "Hardlink created over sensitive file: /etc/shadow - /etc/abc"
+	if message != expectedMessage {
+		t.Fatalf("Message evaluation failed. Expected: %s, Got: %s", expectedMessage, message)
+	}
+
+	// Evaluate the unique id
+	uniqueId, err := celEngine.EvaluateExpression(eventMap, ruleSpec.Rules[0].Expressions.UniqueID)
+	if err != nil {
+		t.Fatalf("Failed to evaluate unique id: %v", err)
+	}
+	expectedUniqueId := "test_/etc/shadow"
+	if uniqueId != expectedUniqueId {
+		t.Fatalf("Unique id evaluation failed. Expected: %s, Got: %s", expectedUniqueId, uniqueId)
+	}
+
+	// Test with another sensitive file
+	e.OldPath = "/etc/sudoers"
+	e.NewPath = "/etc/xyz"
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.HardlinkEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation should trigger for /etc/sudoers")
+	}
+
+	// Test with non-sensitive file (should not trigger)
+	e.OldPath = "/etc/abc"
+	e.NewPath = "/etc/xyz"
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.HardlinkEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if ok {
+		t.Fatalf("Rule evaluation should not trigger for non-sensitive file")
+	}
+
+	// Test with profile - since profile is optional, we can test policy validation
+	profile := objCache.ApplicationProfileCache().GetApplicationProfile("test")
+	if profile == nil {
+		profile = &v1beta1.ApplicationProfile{}
+		profile.Spec.Containers = append(profile.Spec.Containers, v1beta1.ApplicationProfileContainer{
+			Name: "test",
+			PolicyByRuleId: map[string]v1beta1.RulePolicy{
+				"R1012": {
+					AllowedProcesses: []string{"/usr/sbin/groupadd"},
+				},
+			},
+		})
+	}
+
+	objCache.SetApplicationProfile(profile)
+
+	// Test with whitelisted process
+	e.Comm = "/usr/sbin/groupadd"
+	e.OldPath = "/etc/shadow"
+	e.NewPath = "/etc/abc"
+
+	v := rulemanager.NewRulePolicyValidator(objCache)
+	ok, err = v.Validate(ruleSpec.Rules[0].ID, e.Comm, &profile.Spec.Containers[0])
+	if err != nil {
+		t.Fatalf("Failed to validate rule policy: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule policy validation should return true for whitelisted process")
+	}
+}
---
+++ pkg/rules/r1015-malicious-ptrace-usage/rule_test.go
@@ -0,0 +1,149 @@
+package r1015maliciousptraceusage
+
+import (
+	"testing"
+	"time"
+
+	"github.com/goradd/maps"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"github.com/kubescape/node-agent/pkg/config"
+	tracerptracetype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/ptrace/tracer/types"
+	"github.com/kubescape/node-agent/pkg/objectcache"
+	objectcachev1 "github.com/kubescape/node-agent/pkg/objectcache/v1"
+	celengine "github.com/kubescape/node-agent/pkg/rulemanager/cel"
+	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/cache"
+	"github.com/kubescape/node-agent/pkg/utils"
+	common "github.com/kubescape/rulelibrary/pkg/common"
+)
+
+const (
+	// Define the ptrace constants
+	PTRACE_SETREGS  = 13
+	PTRACE_POKETEXT = 4
+	PTRACE_POKEDATA = 5
+)
+
+func TestR1015MaliciousPtraceUsage(t *testing.T) {
+	ruleSpec, err := common.LoadRuleFromYAML("malicious-ptrace-usage.yaml")
+	if err != nil {
+		t.Fatalf("Failed to load rule: %v", err)
+	}
+
+	// Create a ptrace event
+	e := &tracerptracetype.Event{
+		Event: eventtypes.Event{
+			CommonData: eventtypes.CommonData{
+				K8s: eventtypes.K8sMetadata{
+					BasicK8sMetadata: eventtypes.BasicK8sMetadata{
+						ContainerName: "test",
+					},
+				},
+				Runtime: eventtypes.BasicRuntimeMetadata{
+					ContainerID: "test",
+				},
+			},
+		},
+		Comm:    "malicious_process",
+		Pid:     1234,
+		PPid:    5678,
+		Uid:     1000,
+		Gid:     1000,
+		ExePath: "/path/to/malicious_process",
+		Request: PTRACE_SETREGS, // Malicious ptrace request
+	}
+
+	objCache := &objectcachev1.RuleObjectCacheMock{
+		ContainerIDToSharedData: maps.NewSafeMap[string, *objectcache.WatchedContainerData](),
+	}
+
+	objCache.SetSharedContainerData("test", &objectcache.WatchedContainerData{
+		ContainerType: objectcache.Container,
+		ContainerInfos: map[objectcache.ContainerType][]objectcache.ContainerInfo{
+			objectcache.Container: {
+				{
+					Name: "test",
+				},
+			},
+		},
+	})
+
+	celEngine, err := celengine.NewCEL(objCache, config.Config{
+		CelConfigCache: cache.FunctionCacheConfig{
+			MaxSize: 1000,
+			TTL:     1 * time.Microsecond,
+		},
+	})
+	if err != nil {
+		t.Fatalf("Failed to create CEL engine: %v", err)
+	}
+
+	celSerializer := celengine.CelEventSerializer{}
+
+	eventMap := celSerializer.Serialize(e)
+
+	// Evaluate the rule - should always return true for ptrace events
+	ok, err := celEngine.EvaluateRule(eventMap, utils.PtraceEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation should always return true for ptrace events")
+	}
+
+	// Evaluate the message
+	message, err := celEngine.EvaluateExpression(eventMap, ruleSpec.Rules[0].Expressions.Message)
+	if err != nil {
+		t.Fatalf("Failed to evaluate message: %v", err)
+	}
+	expectedMessage := "Malicious ptrace usage detected from: malicious_process on PID: 1234"
+	if message != expectedMessage {
+		t.Fatalf("Message evaluation failed. Expected: %s, Got: %s", expectedMessage, message)
+	}
+
+	// Evaluate the unique id
+	uniqueId, err := celEngine.EvaluateExpression(eventMap, ruleSpec.Rules[0].Expressions.UniqueID)
+	if err != nil {
+		t.Fatalf("Failed to evaluate unique id: %v", err)
+	}
+	expectedUniqueId := "/path/to/malicious_process_malicious_process"
+	if uniqueId != expectedUniqueId {
+		t.Fatalf("Unique id evaluation failed. Expected: %s, Got: %s", expectedUniqueId, uniqueId)
+	}
+
+	// Test with different ptrace request
+	e.Request = PTRACE_POKETEXT
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.PtraceEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation should always return true for ptrace events regardless of request type")
+	}
+
+	// Test with different process
+	e.Comm = "processA"
+	e.Request = PTRACE_POKEDATA
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.PtraceEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation should always return true for ptrace events regardless of process")
+	}
+
+	// Test with unknown process
+	e.Comm = "unknown_process"
+	eventMap = celSerializer.Serialize(e)
+
+	ok, err = celEngine.EvaluateRule(eventMap, utils.PtraceEventType, ruleSpec.Rules[0].Expressions.RuleExpression)
+	if err != nil {
+		t.Fatalf("Failed to evaluate rule: %v", err)
+	}
+	if !ok {
+		t.Fatalf("Rule evaluation should always return true for ptrace events even for unknown processes")
+	}
+}
